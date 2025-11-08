6a19d9bdf1d330fe269d643c336b98bb0015e852
euclideslins <euclidesvasconcelos01@gmail.com>
2025-11-08 17:35:35 -0300

    fix: update rate limiter time to 5 minuts

--- src/middleware/rate-limiter.ts
+++ src/middleware/rate-limiter.ts
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit';
 
 export const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 25,
+    windowMs: 5 * 60 * 1000,
+    max: 5,
     message: {
         success: false,
         message: 'Too many login attempts, please try again later',
